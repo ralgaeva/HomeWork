9ddabb5a3234fd15825941d43f03653102c856cd
rebiyye <rebiyyealqayeva@gmail.com>
2021-09-28 22:48:34 +0400

    add new file

---
+++ qualitair/main.js
@@ -0,0 +1,8 @@
+function openFunction(){
+    document.getElementById("menu").style.width="1050px";
+    document.getElementById("mainbox").style.marginleft="300px"
+}
+function closeFunction(){
+    document.getElementById("menu").style.width="0px";
+    document.getElementById("mainbox").style.marginleft="0px"
+}
