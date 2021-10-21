5ad7803bce1ff27704f6e10b15a53c59cd3fd035
rebiyye <rebiyyealqayeva@gmail.com>
2021-10-21 19:29:56 +0400

    add new file

---
+++ electronics-woocommerce/main.js
@@ -0,0 +1,8 @@
+function openFunction(){
+    document.getElementById("menu").style.width="250px";
+    document.getElementById("mainbox").style.marginleft="300px"
+}
+function closeFunction(){
+    document.getElementById("menu").style.width="0px";
+    document.getElementById("mainbox").style.marginleft="0px"
+}
